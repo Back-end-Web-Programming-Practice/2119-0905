1042c73f4c9f67bcb4ef1a2363048fee3202a467
gk7734 <gamekiller7734@naver.com>
2024-09-10 11:56:36 +0900

    update

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ const logger = require('morgan');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const nunjucks = require('nunjucks');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes');
 const usersRouter = require('./routes/users');
 
 dotenv.config();
 
 const app = express();
 
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIES_SECRET));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -29,35 +35,43 @@ app.use(session({
     name: 'session-cookie',
 }));
 
-try {
-    fs.readdirSync("uploads");
-} catch (err) {
-    console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
-    fs.mkdirSync("uploads");
-}
+app.use('/', indexRouter);
+app.use('/user', usersRouter);
+
+// try {
+//     fs.readdirSync("uploads");
+// } catch (err) {
+//     console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+//     fs.mkdirSync("uploads");
+// }
+
+// app.use((req, res, next) => {
+//     console.log("모든 요청에서 다 실행됩니다.");
+//     next();
+// });
+//
+// app.use((req, res, next) => {
+//     res.status(404).send("Not Found");
+// })
+//
+// app.use((res, req, next, err) => {
+//     console.error(err);
+//     res.status(500).send(err.message);
+// });
 
 app.use((req, res, next) => {
-    console.log("모든 요청에서 다 실행됩니다.");
-    next();
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
-
-app.get(
-    "/",
-    (req, res, next) => {
-        console.log("GET / 요청에서만 실행됩니다.");
-        next();
-    },
-    (req, res) => {
-        throw new Error("에러는 에러처리 미들웨어로 갑니다.");
-    },
-);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 module.exports = app;
